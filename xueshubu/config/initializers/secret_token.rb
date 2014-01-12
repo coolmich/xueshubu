1b44e828c9caeb7bff6cb88d16dbd85f855d36df
coolmich <coolmich00@gmail.com>
2014-01-11 21:21:20 -0800

    Initial commit

---
+++ xueshubu/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xueshubu::Application.config.secret_key_base = '0cafcb4c41c13debaf0bcd09c07325b08fc1d0cfd9dfa7c43c25d61f43767224013d2c17f5808122a09224b33a977688b01307ea61711b17a903f7b274003ab8'
---
+++ xueshubu/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Xueshubu::Application.config.session_store :cookie_store, key: '_xueshubu_session'
